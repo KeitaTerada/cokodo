2fb8c080ff27f04a70172cf76aa6006f3489c16c
KeitaTerada <keita.terada.work@gmail.com>
2024-02-08 23:34:43 +0900

    [add]vercelのライブラリをインストール

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-import Image from "next/image";
+import { sql } from "@vercel/postgres";
 
-export default function Home() {
-  return <div>test</div>;
+export default async function Cart({
+  params,
+}: {
+  params: { user: string };
+}): Promise<JSX.Element> {
+  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
+
+  return (
+    <div>
+      {rows.map((row) => (
+        <div key={row.id}>
+          {row.id} - {row.quantity}
+        </div>
+      ))}
+    </div>
+  );
 }
