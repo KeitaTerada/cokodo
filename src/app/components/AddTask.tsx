a38010865a551adbad860f435c76c0f1789a30b6
DESKTOP-FUPJ1OI\rikuk <rikukuri88antgtk@gmail.com>
2024-02-12 16:47:29 +0900

    [add]

--- src/app/components/AddTask.tsx
+++ src/app/components/AddTask.tsx
@@ -1,6 +1,37 @@
-import styles from "./AddTask.module.css";
+"use client";
+import { useState } from "react";
 
-export default function AddTask() {
-  /*　タスクの追加を押すと入力画面が表示　*/
-  return <></>;
-}
+type ModalProps = {
+  buttonLabel: string;
+};
+
+const AddTask = ({ buttonLabel }: ModalProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="">
+      <button onClick={toggleModal}>{buttonLabel}</button>
+      {isOpen && (
+        <div className="rounded">
+          <div>
+            <p className="font-bold">タイトル</p>
+            <input></input>
+          </div>
+          <div>
+            <p className="font-bold">タスク詳細</p>
+            <input></input>
+          </div>
+          <div className="rounded-xl bg-blue-400 text-white-400 font-bold">
+            <button onClick={toggleModal}>タスクの追加</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AddTask;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
 import { TodoList } from "./components/TodoList";
 import AddTask from "./components/AddTask";
+import { useState } from "react";
 
 export default function TodoPage() {
+  const [show, setShow] = useState(false);
   return (
     <>
       <div className="w-1/2 mt-48 mx-auto">
-        <p className="font-bold text-6xl">タスク管理</p>
-        <TodoList></TodoList>
-        <div className="bg-gray-100 text-blue-600 text-center">
-          <button>+ タスクの追加</button>
+        <div className="mb-64">
+          <p className="font-bold text-6xl">タスク管理</p>
+          <TodoList></TodoList>
+          <div className="z-2 w-2/5 rounded-2xl bg-[rgba(0, 0, 0, 0.5)]] text-blue-600 text-3xl text-center py-4">
+            <AddTask buttonLabel="+ タスクの追加"></AddTask>
+          </div>
+        </div>
+        <div className="">
+          <p className="font-bold text-4xl">終了済みタスク</p>
         </div>
       </div>
     </>
